7cd9f9383ad2dc7f7bd264140a79ab0b47b2101f
Bagas-AP <dcool8754@gmail.com>
2023-10-16 17:09:39 +0700

    kurang chat and speak up

--- controller/home.go
+++ controller/home.go
@@ -20,4 +20,24 @@ func Home(db *gorm.DB, q *gin.Engine) {
 
 		utils.HttpRespSuccess(c, http.StatusOK, "Success get all posts", posts)
 	})
+
+	r.GET("/popular", middleware.Authorization(), func(c *gin.Context) {
+		var posts []model.Post
+		if err := db.Order("\"like\" DESC").Find(&posts).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "Success get all posts", posts)
+	})
+
+	r.GET("/latest", middleware.Authorization(), func(c *gin.Context) {
+		var posts []model.Post
+		if err := db.Order("created_at desc").Find(&posts).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "Success get all posts", posts)
+	})
 }
