c628e26b3d3fb3f452c5368eb080317bf219ee3f
Bagas-AP <dcool8754@gmail.com>
2023-11-03 00:20:13 +0700

    changed avatar links

--- utils/default_avatar.go
+++ utils/default_avatar.go
@@ -2,15 +2,15 @@ package utils
 
 func DefaultAvatar(input string) string {
 	avatarLinks := map[string]string{
-		"1": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/1.png",
-		"2": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/2.png",
-		"3": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/3.png",
-		"4": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/4.png",
-		"5": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/5.png",
-		"6": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/6.png",
-		"7": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/7.png",
-		"8": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/8.png",
-		"9": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/9.png",
+		"1": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_1.png",
+		"2": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_2.png",
+		"3": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_3.png",
+		"4": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_4.png",
+		"5": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_5.png",
+		"6": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_6.png",
+		"7": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_7.png",
+		"8": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_8.png",
+		"9": "https://pbhlfzbgpnywavonhdld.supabase.co/storage/v1/object/public/images/avatar_9.png",
 	}
 
 	if link, exists := avatarLinks[input]; exists {
